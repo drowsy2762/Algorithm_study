2589300f3def4d8cb1ce5a5beffcc9c507539259
drowsy2762 <drowsy2762@gmail.com>
2026-04-18 21:13:16 +0900

    clear

--- Python/20000/10001-11000/10775.py
+++ Python/20000/10001-11000/10775.py
@@ -1,38 +1,41 @@
 # https://www.acmicpc.net/problem/10775 : 공항(python3)
 # 2026-04-18
 import sys
-sys.setrecursionlimit(10**6)
 
 def solution():
-    input = sys.stdin.readline
+    input_data = sys.stdin.read().split()
+    if not input_data:
+        return
     
+    g_count = int(input_data[0])
+    p_count = int(input_data[1])
+    
+    parent = list(range(g_count + 1))
+
     def find_parent(x):
-        if parent[x] != x:
-            parent[x] = find_parent(parent[x])
-        return parent[x]
-    
-    def union_parent(x, y):
-        x = find_parent(x)
-        y = find_parent(y)
-        if x < y:
-            parent[y] = x
-        else:
-            parent[x] = y
-
-    g = int(input())
-    p = int(input())
-
-    parent = [i for i in range(g + 1)]
-    plane = []
-    for _ in range(p):
-        plane.append(int(input()))
+        root = x
+        while parent[root] != root:
+            root = parent[root]
+        
+        while parent[x] != root:
+            next_node = parent[x]
+            parent[x] = root
+            x = next_node
+            
+        return root
 
     cnt = 0
-    for p in plane:
-        x = find_parent(p)
-        if x == 0:
+    for i in range(2, p_count + 2):
+        gi = int(input_data[i])
+        root = find_parent(gi)
+
+        if root == 0:
             break
-        union_parent(x, x - 1)
+        
+        parent[root] = find_parent(root - 1)
         cnt += 1
     
     print(cnt)
+
+if __name__ == "__main__":
+    solution()
