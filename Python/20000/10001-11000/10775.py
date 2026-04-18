6348ecf840f24863de295f74fa0fb54257a9af19
drowsy2762 <drowsy2762@gmail.com>
2026-04-18 15:44:27 +0900

    clear

---
+++ Python/20000/10001-11000/10775.py
@@ -0,0 +1,38 @@
+# https://www.acmicpc.net/problem/10775 : 공항(python3)
+# 2026-04-18
+import sys
+sys.setrecursionlimit(10**6)
+
+def solution():
+    input = sys.stdin.readline
+    
+    def find_parent(x):
+        if parent[x] != x:
+            parent[x] = find_parent(parent[x])
+        return parent[x]
+    
+    def union_parent(x, y):
+        x = find_parent(x)
+        y = find_parent(y)
+        if x < y:
+            parent[y] = x
+        else:
+            parent[x] = y
+
+    g = int(input())
+    p = int(input())
+
+    parent = [i for i in range(g + 1)]
+    plane = []
+    for _ in range(p):
+        plane.append(int(input()))
+
+    cnt = 0
+    for p in plane:
+        x = find_parent(p)
+        if x == 0:
+            break
+        union_parent(x, x - 1)
+        cnt += 1
+    
+    print(cnt)
