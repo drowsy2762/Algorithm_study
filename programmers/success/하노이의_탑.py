9adba4553f3cf0f93634679984f0d551ec9ef4e2
drowsy2762 <drowsy2762@gmail.com>
2026-05-07 21:56:54 +0900

    clear

---
+++ programmers/success/하노이의_탑.py
@@ -0,0 +1,18 @@
+# 2026-05-07
+
+
+def solution(n):
+    answer = []
+
+    def hanoi(n, start, end, mid):
+        if n == 1:
+            answer.append([start, end])
+            return
+
+        hanoi(n - 1, start, mid, end)
+        answer.append([start, end])
+        hanoi(n - 1, mid, end, start)
+
+    hanoi(n, 1, 3, 2)
+
+    return answer
