1c889a740e3a2f10d10d8578afc1d55daf06f250
drowsy2762 <drowsy2762@gmail.com>
2026-08-26 13:04:46 +0900

    2026-08-26(clear)

---
+++ programmers/success/스킬트리.py
@@ -0,0 +1,19 @@
+# 2026-08-26
+
+
+def solution(skill, skill_trees):
+    skill_set = set(skill)
+    valid_count = 0
+    for tree in skill_trees:
+        skill_idx = 0
+        is_valid = True
+        for s in tree:
+            if s in skill_set:
+                if s == skill[skill_idx]:
+                    skill_idx += 1
+                else:
+                    is_valid = False
+                    break
+        if is_valid:
+            valid_count += 1
+    return valid_count
